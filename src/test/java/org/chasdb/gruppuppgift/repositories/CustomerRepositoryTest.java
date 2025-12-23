ed2aa1009c2953778a801d810e1b25c31b8a4cf9
Joso1207 <joso1207@student.miun.se>
2025-12-23 11:43:47 +0100

    Ensuring consistent application-config.

--- src/test/java/org/chasdb/gruppuppgift/repositories/CustomerRepositoryTest.java
+++ src/test/java/org/chasdb/gruppuppgift/repositories/CustomerRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CustomerRepositoryTest {
 
     @Autowired
