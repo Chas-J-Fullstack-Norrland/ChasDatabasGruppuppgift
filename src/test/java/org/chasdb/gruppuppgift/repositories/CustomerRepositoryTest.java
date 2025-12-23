a45fbcc1e59bc0dadc498d1dc7fbcddd9f60a34d
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2025-12-23 11:13:28 +0100

    Customer Test

--- src/test/java/org/chasdb/gruppuppgift/repositories/CustomerRepositoryTest.java
+++ src/test/java/org/chasdb/gruppuppgift/repositories/CustomerRepositoryTest.java
@@ -4,13 +4,16 @@ import org.chasdb.gruppuppgift.models.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CustomerRepositoryTest {
 
     @Autowired
@@ -21,6 +24,7 @@ public class CustomerRepositoryTest {
         Customer c = new Customer();
         c.setName("Test User");
         c.setEmail("test.user@example.com");
+        c.setCreatedAt(LocalDateTime.now());
 
         Customer saved = customerRepository.save(c);
 
@@ -38,9 +42,12 @@ public class CustomerRepositoryTest {
         Customer c1 = new Customer();
         c1.setName("User One");
         c1.setEmail("test.user1@example.com");
+        c1.setCreatedAt(LocalDateTime.now());
         customerRepository.save(c1);
         List<Customer> all = customerRepository.findAll();
         assertThat(all).hasSize(1);
     }
+
+
 
 }
