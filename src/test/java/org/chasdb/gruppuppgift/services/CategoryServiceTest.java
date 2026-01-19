0ca83b964e58da13d3f0e61fb7b614efabb9bfa7
Joso1207 <joso1207@student.miun.se>
2026-01-19 02:02:45 +0100

    Better test isolation for CategoryServiceTest

--- src/test/java/org/chasdb/gruppuppgift/services/CategoryServiceTest.java
+++ src/test/java/org/chasdb/gruppuppgift/services/CategoryServiceTest.java
@@ -1,12 +1,15 @@
 package org.chasdb.gruppuppgift.services;
 
 import jakarta.transaction.Transactional;
+import org.chasdb.gruppuppgift.cli.AppRunner;
 import org.chasdb.gruppuppgift.repositories.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,12 +19,15 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class CategoryServiceTest {
 
+    @MockitoBean
+    AppRunner appRunner;
     @Autowired
     CategoryService service;
 
 
 
     @Test
+    @Transactional
     void savesAndListsCategories(){
         service.newCategory("Test 1");
         service.newCategory("Test 2");
